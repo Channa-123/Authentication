b484cf5c2dd62ac0437ad35f8481e3e9caa6b894
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-02-06 22:35:09 +0530

    Update SignIn

--- react-firebase-auth/src/components/auth/SignIn.jsx
+++ react-firebase-auth/src/components/auth/SignIn.jsx
@@ -1,26 +1,27 @@
 import {signInWithEmailAndPassword } from "firebase/auth";
-import React,{useState} from 'react'
+import React,{useState} from 'react';
 import {auth} from "../../firebase";
 
 const SignIn = () => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState();  
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");  
 
     const signIn = (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
+      e.preventDefault();
+      console.log(email, password)
+      signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            console.log(userCredential);
+          console.log(userCredential);
         })
         .catch ((error) => {
-                console.log(error);
-            });
+          console.log(error);
+        });
 
     };
   return (
     <div className ="sign-in-container"> 
       <form onSubmit={signIn}>
-        <h1> Log In </h1>
+        <h1> Log In to Your Account </h1>
         <input type="email" placeholder ="Enter your email" value ={email} onInput={(e) => setEmail(e.target.value)}></input>
         <input type="password" placeholder="Enter your password" value={password} onInput={(e) => setPassword(e.target.value)}></input>
         <button type ="submit"> Log In </button>
